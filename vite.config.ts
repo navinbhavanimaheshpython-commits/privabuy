8d7979cf1bbeb03f543c2b9e766aae7937f59df7
Navin Bhavani Mahesh <navinbhavanimaheshpython@gmail.com>
2026-04-10 10:49:59 -0400

    Initial commit - PrivaBuy

---
+++ vite.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  esbuild: {
+    logOverride: { 'this-is-undefined-in-esm': 'silent' }
+  }
+})
