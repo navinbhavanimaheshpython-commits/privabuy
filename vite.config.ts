86a007817532db540417c906a19ae10008187d2f
navinbhavanimaheshpython-commits <navinbhavanimaheshpython@gmail.com>
2026-05-26 02:58:05 -0400

    add sitemap

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import sitemap from 'vite-plugin-sitemap'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sitemap({
+      hostname: 'https://privabuy.com',
+      routes: ['/'],
+    }),
+  ],
   esbuild: {
     logOverride: { 'this-is-undefined-in-esm': 'silent' }
   }
