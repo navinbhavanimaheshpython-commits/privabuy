0c025e710964ac8e6dc0b387289107557a416fc8
navinbhavanimaheshpython-commits <navinbhavanimaheshpython@gmail.com>
2026-05-30 21:31:13 -0400

    fix build with copy-files script

---
+++ copy-files.js
@@ -0,0 +1,9 @@
+﻿const fs = require('fs');
+['app', 'portal', 'dealer-signup', 'reset-password'].forEach(f => {
+  try {
+    fs.copyFileSync('public/' + f + '.html', 'dist/' + f + '.html');
+    console.log('Copied ' + f + '.html');
+  } catch(e) {
+    console.log('Skipped ' + f + '.html');
+  }
+});
